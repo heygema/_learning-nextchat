c9942e02a114003de08fd32780cb6c3b0f407f68
Gema <gem.anggada@gmail.com>
2020-11-26 16:37:35 +0700

    hotfix(server.js): now runs with minimal config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const {createServer} = require('http');
-const createIO = require('socket.io');
-const next = require('next');
+const express = require("express");
+const { createServer } = require("http");
+const createIO = require("socket.io");
+const next = require("next");
 
 class Server {
   app;
@@ -12,35 +12,40 @@ class Server {
   PORT = 3000;
   constructor(port) {
     this.init();
-    this.PORT = port || PORT;
-    this.io.on('connection', this.connectionCallback);
+    this.PORT = port || this.PORT;
+    this.io.on("connection", this.onConnection);
   }
 
   init = () => {
     this.app = express();
     this.server = createServer(this.app);
     // @ts-ignore
-    this.io = createIO();
-    const dev = process.env.NODE_ENV !== 'production';
-    this.nextApp = next({dev});
+    this.io = createIO(this.server);
+    const dev = process.env.NODE_ENV !== "production";
+    this.nextApp = next({ dev });
   };
 
-  connectionCallback = (socket) => {
-    socket.on('chat', (thing) => {
+  onConnection = socket => {
+    socket.on("chat", thing => {
       console.log(`chat from client ${socket.id} >> ${thing} `);
     });
+
+    for (let i = 0; i < 10; i++) {
+      console.log('ran', i);
+      socket.emit("chat", i);
+    }
   };
 
-  start = (callback) => {
+  start = callback => {
     const nextHandler = this.nextApp.getRequestHandler();
 
     this.nextApp.prepare().then(() => {
-      this.app.get('*', (req, res) => {
+      this.app.get("*", (req, res) => {
         nextHandler(req, res);
       });
 
       this.server.listen(this.PORT, () => {
-        console.log('> Server starting on http:.//localhost:', this.PORT);
+        console.log("> Server starting on http:.//localhost:", this.PORT);
         callback && callback(this.PORT);
       });
     });
